6e84cfe0366735473ee92d2459ba92f17462333e
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-25 00:05:25 +0100

    Adicionando a estrutura do componente

---
+++ src/components/marcas/index.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Marcas(){
+    return(
+        <>
+            <div>
+                <h2>As nossas marcas de confiança</h2>
+
+                <div className="container-marca">
+                    <ul>
+                        <li><img src="" alt="" /> </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Marcas;
