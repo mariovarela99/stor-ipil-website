a7c2472c77fb1034d8a5ab9c961999a8875e95f4
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-25 00:24:13 +0100

    Adicionando as logos

--- src/components/marcas/index.jsx
+++ src/components/marcas/index.jsx
@@ -1,14 +1,21 @@
 import React from "react"
 
+
+import logoHuawei from "../../assets/logo-huawei.png"
+
 function Marcas(){
     return(
         <>
-            <div>
+            <div className="marcas">
                 <h2>As nossas marcas de confiança</h2>
 
                 <div className="container-marca">
                     <ul>
-                        <li><img src="" alt="" /> </li>
+                        <li><img src={logoHuawei} alt="logoHuawei" /> </li>
+                        <li><img src={logoHuawei} alt="logoHuawei" /> </li>
+                        <li><img src={logoHuawei} alt="logoHuawei" /> </li>
+                        <li><img src={logoHuawei} alt="logoHuawei" /> </li>
+                        <li><img src={logoHuawei} alt="logoHuawei" /> </li>
                     </ul>
                 </div>
             </div>
