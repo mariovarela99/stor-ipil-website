659d1f1d0e5157ba6500d22854a6debf99d2b12a
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-25 07:32:50 +0100

    (Created)->Componente frame criado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer1";
 import CardProduto from "../components/cardProduto1";
-
+import Frame from "../components/frame3"
 function Home() {
   return (
     <div>
       <Header />
       <CardProduto />
+      <Frame />
       <Footer />
     </div>
   );
