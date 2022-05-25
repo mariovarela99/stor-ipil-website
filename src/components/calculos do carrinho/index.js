aea3e6a409b3749b65ddca1eef7d412eff64d478
Opselicandro.Sousa <opselicandro.sousa@alunos.unis.edu.br>
2022-05-25 19:34:23 -0300

    tabela d carrinho

--- src/components/calculos do carrinho/index.js
+++ src/components/calculos do carrinho/index.js
@@ -5,17 +5,7 @@ function Calculodocarrinho() {
   return (
     <>
       <div>
-            <table class="tab_prod">
-                <tr>
-                    <td><b>TOTAL DE PRODUTOS</b></td><td><b>2</b></td>
-                </tr>
-                <tr>
-                    <td><b>TOTAL</b></td><td><b>250.000,00 Kzs</b></td>
-                </tr>
-                <tr>
-                    <td colspan="2" class="info_preco">Preço com IVA incluindo à taxa em vigor excepto livros</td>
-                </tr>
-            </table>
+            
       </div>
     </>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "../components/header";
 
+
 function Cart() {
   return (
     <>
       <Header />
+      <section>
+      <table class="tab_prod" border="2">
+                <tr>
+                    <td><b>TOTAL DE PRODUTOS</b></td><td><b>2</b></td>
+                </tr>
+                <tr>
+                    <td><b>TOTAL</b></td><td><b>250.000,00 Kzs</b></td>
+                </tr>
+                <tr>
+                    <td colspan="2" class="info_preco">Preço com IVA incluindo à taxa em vigor excepto livros</td>
+                </tr>
+            </table>
+      </section>
     </>
   );
 }
