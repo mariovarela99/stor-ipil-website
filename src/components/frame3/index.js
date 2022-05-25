6e56460e646fe89090118846bee90c4b47b4baaa
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-25 01:25:06 +0100

    (Started)-> Iniciando comoponente frame3

---
+++ src/components/frame3/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "style.css"
+
+function Frame(){
+    return(
+        <>
+            <div className="frame">
+                <div className="frame-image">
+                    <img src="" alt="" />
+                </div>
+
+                <h3>Asus Portátil Gaming TUF Gaming F15 TUF506LH-HN218 15.6´´ i5-1030...</h3>
+                <p>283995 Kz</p>
+            </div>
+        </>
+    )
+}
+
+export default Frame;
