e1f87488d71dccd873993bec7f14743ba92ad7d6
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-25 09:31:16 +0100

    (Created)-> List of frames createad

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,13 @@ import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer1";
 import CardProduto from "../components/cardProduto1";
-import Frame from "../components/frame3"
+import ListFrame from "../components/listFrame"
 function Home() {
   return (
     <div>
       <Header />
       <CardProduto />
-      <Frame />
+      <ListFrame quantity={4} />
       <Footer />
     </div>
   );
