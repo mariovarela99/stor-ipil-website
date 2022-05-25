1b9a9b04f0adb94730849967a07ef87af462e92c
mariovarela99 <mariovarela99@outlook.com>
2022-05-25 19:03:43 +0100

    Footer 1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,30 +4,35 @@ import CardProduto from "../../components/cardProduto1";
 import Banner from "../../components/banner";
 import { CardsContainer } from "./style";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import Footer from "../../components/footer1";
 
 function Home() {
   return (
-    <div>
-      <Header />
-      <Banner />
-      <CardsContainer>
-        <h2>Em Destaque</h2>
-        <div>
-          <button type="button">
-            <FaAngleLeft size={24} />
-          </button>
-          <ul>
-            <CardProduto />
-            <CardProduto />
-            <CardProduto />
-            <CardProduto />
-          </ul>
-          <button type="button">
-            <FaAngleRight size={24} />
-          </button>
-        </div>
-      </CardsContainer>
-    </div>
+    <>
+      <div>
+        <Header />
+        <Banner />
+        <CardsContainer>
+          <h2>Em Destaque</h2>
+          <div>
+            <button type="button">
+              <FaAngleLeft size={24} />
+            </button>
+            <ul>
+              <CardProduto />
+              <CardProduto />
+              <CardProduto />
+              <CardProduto />
+            </ul>
+            <button type="button">
+              <FaAngleRight size={24} />
+            </button>
+          </div>
+        </CardsContainer>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
