86eff4bc7a4e184ef42d65dcd4d0d67817bba916
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-31 10:41:55 +0100

    Resolving comflits in marcas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import Routing from "./routes";
-<<<<<<< HEAD
-import "./styles/globalStyles.css"
-=======
 import "./styles/globalStyles.css";
->>>>>>> main
 
 function App() {
   return <Routing />;
