12f66c7b07a4c4b973fb69ed9af53fd29ac0d07b
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-05-31 16:09:06 +0100

    Carrinho Terminado

--- src/pages/carrinho/index.js
+++ src/pages/carrinho/index.js
@@ -24,7 +24,7 @@ function Carrinho(){
                         <CarrinhoTabela />
                     </div>
                     <BtnNext />
-                </div>
+                </div> 
             </div>
         </>
     )
