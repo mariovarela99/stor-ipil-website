fea518481fb41caae9dfe3b47fb3f5924187612a
Osvaldo de Sousa <osvaldodesousa9432@gmail.com>
2022-06-01 07:06:30 +0100

    (Conflit)-> containerDosCardsConflitos resolvido...

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import Header from "../../components/header";
 import CardProduto from "../../components/cardProduto1";
 import Banner from "../../components/banner";
-<<<<<<< HEAD
 import CardMaisVendidos from "../../components/conteinerdoscards"
-=======
 import Newsletter from "../../components/newsletter"
->>>>>>> main
 import Marcas from "../../components/marcas"
 import { CardsContainer } from "./style";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
@@ -33,13 +30,10 @@ function Home() {
           </button>
         </div>
       </CardsContainer>
-<<<<<<< HEAD
       <CardMaisVendidos />
-=======
       <Newsletter />
->>>>>>> main
       <Marcas />
-    </div>
+    </div> 
   );
 }
 
