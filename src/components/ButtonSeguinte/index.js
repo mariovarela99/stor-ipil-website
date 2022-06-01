29fbb50c0990a5ea0f823ec9913a6c903daadb1b
AODavid <bernabedavid12@gmail.com>
2022-06-01 21:30:21 +0100

    Meu segundo commit footer1

--- src/components/ButtonSeguinte/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const ButtonSeguinte = () => {
-  return <div> Card Produto 2 </div>;
-};
-
-export default ButtonSeguinte;
---
+++ src/components/Button_Seguinte/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+const ButtonSeguinte = () => {
+  return (
+    <>
+      <div class="main">
+        <div class="Button_Seguinte">
+          <button>Seguinte</button>
+        </div>
+    </div>
+    </>
+  );
+};
+
+export default ButtonSeguinte;
